2e3d60f347675afbda08797a4f7df9251e1f934d
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-07-08 00:00:32 +0700

    lecture_1

---
+++ lecture_1.py
@@ -0,0 +1,66 @@
+# value = None
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+# # value = 12345
+# # print(value)
+# # #type увидеть тип переменной
+# # print(type(a))
+# # print(type(b))
+# # print(type(value))
+
+# s = 'Hello World'
+# #s = 'Hello \nWorld' #\n каждая строка
+# print(s) #вывод строки
+# print(a, '-',b,'+',s)
+# #print('{} - {} + {}'.format(a,b,s))
+# print(f'{s} - {b} + {a}')
+# print('{2} - {0} + {1}'.format(a,b,s))
+
+#f = True #False
+#print(f)
+
+# list = ['1','2','3','hello',4.5,True]
+# print(list)
+
+# print('Введите а')
+# # a = input
+# print('Введите b')
+# b = input
+# print( a,b)
+# a = int(input())
+# b = float(input())
+# print( a, '+' ,b, '=', a+b)
+
+# a = 123
+# b = -321
+# c = a+b
+# print(c)
+
+# a = 12
+# b = 8
+# c = a/b #%- остаток от деления ** - возведение в степень
+# print(c)
+
+# a = 1.3323
+# b = 3 
+# c = round(a*b,5) #Точность знаков после запятой  
+
+# a= 3
+# # a  = a +5
+# a +=6  #аналогично и для других операций
+# print(a)
+
+# a = 1>4
+# print(a)
+
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+
+# a = [1,2]
+# b = [1,2]
+# print(a == b)
+# a = 1<3<5< #можно использовать тройныи и более не равенства
+# print(a)
